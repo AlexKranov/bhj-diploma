715973fcd7d63301888592f5b95d7b4117d87085
AlexKranov <86122490+AlexKranov@users.noreply.github.com>
2022-01-10 13:23:57 +0300

    update

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -3,12 +3,19 @@
  * создания нового счёта
  * */
 class CreateAccountForm extends AsyncForm {
-  /**
-   * Создаёт счёт с помощью Account.create и закрывает
-   * окно в случае успеха, а также вызывает App.update()
-   * и сбрасывает форму
-   * */
-  onSubmit(data) {
-
-  }
+    /**
+     * Создаёт счёт с помощью Account.create и закрывает
+     * окно в случае успеха, а также вызывает App.update()
+     * и сбрасывает форму
+     * */
+    onSubmit(data) {
+        Account.create(data, (err, response) => {
+            if (response && response.success) {
+                const modalWindow = App.getModal('createAccount');
+                modalWindow.close();
+                App.update();
+                this.element.reset();
+            }
+        })
+    }
 }
